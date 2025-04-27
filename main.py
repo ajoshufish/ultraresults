2888894c9d37ed0c7d6d9eaa5acd17d2e2dc4947
Joshu Fisher <joshu.fisher@gmail.com>
2025-04-27 15:39:02 -0700

    Single event v1, multi-day event support, international loc support v1

---
+++ main.py
@@ -0,0 +1,4 @@
+from single_event import single_event_scrape
+
+url='https://statistik.d-u-v.org/getresultevent.php?event=107971'
+single_event_scrape(url)
--- single_event.py
+++ single_event.py
@@ -1,116 +1,167 @@
 #imports
 import pandas as pd
 import lxml
+import openpyxl
+import re
 
-#Establish URL for particular event, and pull
-url = 'https://statistik.d-u-v.org/getresultevent.php?event=104431'
-tables = pd.read_html(url)
 
-#parse results into an initial table
-tmp = pd.read_html(url, extract_links='body')[5]['Surname, first name'].astype(str)
-tmp.columns = ['Name']
-
-#separate out event information
-event_info = tables[3]
-event_title = event_info[1][1]
-event_distance = event_info[1][2]
-event_date = event_info[1][0]
-
-#separate out the results table, including athlete IDs from the relevant URLs
-results_info = tables[5].iloc[:, 0:10]
-results_info.columns = ['Rank', 'Time', 'Name', 'City', 'Nation', 'YOB', 'Gender', 'Gender_Rank', 'Category', 'Cat_Rank']
-ath_ids = pd.DataFrame(pd.DataFrame(tmp['Name'].str.split('=').tolist())[1].str.split('\'').tolist())[0]
-results_info['Ath_ID'] = ath_ids
-
-#convert result time to seconds as a unified standard, to be expanded once processing multi-day-centered events 
-if(results_info.at[0,'Time'][-1:] == 'h'): #based on hours, format= hh:mm:ss
-    hrs = pd.DataFrame(results_info['Time'].str.slice(0,2)).astype(int)
-    mins = pd.DataFrame(results_info['Time'].str.slice(3,5)).astype(int)
-    secs = pd.DataFrame(results_info['Time'].str.slice(6,8)).astype(int)
-    duration = (hrs * 3600) + (mins * 60) + (secs)
-
-results_info['Duration'] = duration
-
-# Add Event Info to database (if necessary) -- search existing event list for the the title
-path = 'ultra results.xlsx' # we start with a local db
-wb = openpyxl.load_workbook(path)
-esh = wb['Event_Info']
-evs = pd.DataFrame(esh.values, columns=next(esh.values)[0:])
-rsh = wb['Race_Info']
-rcs = pd.DataFrame(rsh.values, columns=next(rsh.values)[0:])
-ressh = wb['Results']
-resdf = pd.DataFrame(ressh.values, columns=next(ressh.values)[0:])
-partsh = wb['Participant_Info']
-partdf = pd.DataFrame(partsh.values, columns=next(partsh.values)[0:])
-
-row = []
-if(len(evs['id']) > 1): #do we have anything in the list already?
+# Function to scrape results information from a single event (race) URL. Input is the URL to scrape. It reads the
+# information from that URL and adds the event, race, participant, and results information to the database.
+def single_event_scrape(url):
+    #url = 'https://statistik.d-u-v.org/getresultevent.php?event=104431' #testing url
     
-    #yes, so let's search it
-    if(max(evs['Title'].str.find(event_title)) < 0):
-    #did not find it, so let's add it
+    #Pull basic information from the passed URL
+    tables = pd.read_html(url)
+    
+    tmp = pd.read_html(url, extract_links='body')[5][pd.read_html(url, extract_links='body')[5].columns[2][0]].astype(str)
+    tmp.columns = ['Name']
+    ath_ids = pd.DataFrame(pd.DataFrame(tmp['Name'].str.split('=').tolist())[1].str.split('\'').tolist())[0]
+    event_info = tables[3]
+    results_info = tables[5].iloc[:, 0:10]
+
+    race_distance = event_info[1][2].split(' ', 1)[0]
+    event_title = event_info[1][1].split(' ', 1)[1].rsplit(' ', 2)[0] #culls out which iteration of the race this is
+    if(len(event_info[1][0]) > 10):
+        #multiday, take just the first day as the date the event took place
+        race_startdate = event_info[1][0].split('.', 1)[0] + '.'+ event_info[1][0].split('.', 1)[1].split('.', 1)[1]
+    else:
+        race_startdate = event_info[1][0] 
+    race_year = pd.to_numeric(race_startdate.rsplit('.',1)[1])
+
+    results_info.columns = ['Rank', 'Time', 'Name', 'City', 'Nation', 'YOB', 'Gender', 'Gender_Rank', 'Category', 'Cat_Rank']
+    results_info['Ath_ID'] = ath_ids
+
+    times = results_info['Time']
+    duration = pd.Series([])
+    for i in range(len(times)):
+        entry = times.iloc[i]
+        if(re.search(r'^[0-9]+d', entry)): #multi-day time
+            days = int(entry.split()[0].split('d')[0])
+            hrs = int(entry.split()[1].split(':')[0])
+            mins = int(entry.split()[1].split(':')[1])
+            secs = int(entry.split()[1].split(':')[2])
+        else:
+            days = 0
+            hrs = int(entry.split(':')[0])
+            mins = int(entry.split(':')[0])
+            secs = int(entry.split(':')[0])
+        time = secs + (60 * mins) + (3600 * hrs) + (86400 * days)
+        duration[i] = time
+    duration.name = 'Duration'
+    results_info = pd.concat([results_info, duration], axis = 1)
+
+    path = 'ultra results.xlsx' # we start with a local db
+
+    #gather existing info from the database
+    wb = openpyxl.load_workbook(path)
+    esh = wb['Event_Info']
+    evs = pd.DataFrame(esh.values, columns=next(esh.values)[0:])
+    rsh = wb['Race_Info']
+    rcs = pd.DataFrame(rsh.values, columns=next(rsh.values)[0:])
+    ressh = wb['Results']
+    resdf = pd.DataFrame(ressh.values, columns=next(ressh.values)[0:])
+    partsh = wb['Participant_Info']
+    partdf = pd.DataFrame(partsh.values, columns=next(partsh.values)[0:])
+
+    # Start by processing the Event aspect. Is it already in the database? If not, we gather relevant
+    # event information from the event's basic information URL and then add it to our database. 
+    # If it is found already then we're good.
+    row = []
+    if(len(evs['event_id']) > 1): #do we have anything in the list already?
+    
+        #yes, so let's search it for the event
+        if(max(evs['title'].str.find(event_title)) < 0):
+        #did not find it, so let's add it
+            evid = max(evs['event_id'][1:]) + 1
+        
+            #first, we need more info about the event -- set up URL for getting more complete event info
+            etmp = 'https://statistik.d-u-v.org/' + pd.read_html(url, extract_links='body')[3][2][1][1]
+            etab = pd.read_html(etmp)
+
+            #location info
+            ev_location = etab[2][1][etab[2][0].str.find('Start in').idxmax()]
+            ev_country = ev_location.rsplit('(', maxsplit=1)[1].split(')')[0]
+            if(ev_country == 'USA'):
+                ev_state = ev_location.rsplit(' ', maxsplit=2)[1]
+                ev_city = ev_location.rsplit(' ', maxsplit=2)[0].split(',')[0]
+            else: 
+                ev_city = ev_location.rsplit(' ', maxsplit=1)[0]
+                ev_state = 'N/A'
+            ev_url = etab[2][1][etab[2][1].str.find('http').idxmax()]
+
+            row = [evid, event_title, ev_city, ev_state, ev_country, ev_url]
+
+            if len(row) > 0: #append it to table
+                esh.append(row)
+                wb.save(path)
+        
+            evs.loc[evid] = row #update our internal event list
+        else: evid = evs['title'].str.contains(event_title).idxmax() #found it
+    else:
+        #no, so let's start the list. as above, gather basic info and add it (as first entry)
+        evid = 1
         #first, we need more info about the event -- set up URL for getting more complete event info
         etmp = 'https://statistik.d-u-v.org/' + pd.read_html(url, extract_links='body')[3][2][1][1]
         etab = pd.read_html(etmp)
 
         #location info
-        ev_city = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[0].split(',')[0]
-        ev_state = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[0].split(', ')[1]
-        ev_country = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[1].replace('(', '').replace(')', '')
+        ev_location = etab[2][1][etab[2][0].str.find('Start in').idxmax()]
+        ev_country = ev_location.rsplit('(', maxsplit=1)[1].split(')')[0]
+        if(ev_country == 'USA'):
+            ev_state = ev_location.rsplit(' ', maxsplit=2)[1]
+            ev_city = ev_location.rsplit(' ', maxsplit=2)[0].split(',')[0]
+        else: 
+            ev_city = ev_location.rsplit(' ', maxsplit=1)[0]
+            ev_state = 'N/A'
         ev_url = etab[2][1][etab[2][1].str.find('http').idxmax()]
-        newid = max(evs['id'][1:]) + 1
-        row = [newid, event_title, ev_city, ev_country, ev_url]
-else:
-    #no, so let's add to it
-    etmp = 'https://statistik.d-u-v.org/' + pd.read_html(url, extract_links='body')[3][2][1][1]
-    etab = pd.read_html(etmp)
 
-    #location info
-    ev_city = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[0].split(',')[0]
-    ev_state = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[0].split(', ')[1]
-    ev_country = etab[2][1][etab[2][0].str.find('Start in').idxmax()].rsplit(' ', 1)[1].replace('(', '').replace(')', '')
-    ev_url = etab[2][1][etab[2][1].str.find('http').idxmax()]
-    newid = 1
-    row = [newid, event_title, ev_city, ev_state, ev_country, ev_url]
+        row = [evid, event_title, ev_city, ev_state, ev_country, ev_url]
 
-if len(row) > 0: #append it
-    esh.append(row)
-    wb.save(path)
+        if len(row) > 0: #append it to table
+            esh.append(row)
+            wb.save(path)   
+        evs.loc[evid] = row #update our internal event list 
 
-# Add Race Info to database (if necessary) -- search existing event list for matching event title to get event id, then pair with year of results to search event id + year pairs in race list
-if(max(evs['Title'].str.find(event_title))) > -1: #if we find the race
-    evid = evs['id'][evs['Title'].str.find(event_title).idxmax()] #get the race id
-    if len(rcs[(rcs['Event_ID'] == evid) & (rcs['Year'] == race_year) ]) == 0: #we have the event, but not this instance
-        if(len(rcs['id']) > 1):
-            newid = max(rcs['id'][1:]) + 1
+    # Next we add Race Info to database (if necessary) -- search existing event list for matching event title to get event id, 
+    # then pair with year of results to search event id + year pairs in race list
+
+    evid = evs['event_id'][evs['title'].str.find(event_title).idxmax()] #get the event id
+    if len(rcs[(rcs['event_id'] == evid) & (rcs['year'] == race_year) ]) == 0: #we have the event, but not this instance/race
+        if(len(rcs['race_id']) > 1): #are we starting the race list from scratch or not
+            race_id = max(rcs['race_id'][1:]) + 1 #no, so increment
         else:
-            newid = 1
-        rsh.append([newid, evid, race_distance, race_year])
+            race_id = 1
+        rsh.append([race_id, evid, race_distance, race_year])
         wb.save(path)
 
-# Now to add participants and results -- for each item in the result table:
-# 1) Check the participant table to see if we want to write a new participant entry based on the ath_id, if we do then write it
-# 2) Check the results table for race id + ath_id pairs. if not found, write a result entry.
-resid = 1
-for i in range(len(results_info)): #for each result
-    entry = results_info.iloc[i]
-    #does participant already exist in our dataset?
-    if(not partdf['ath_id'].eq(entry['Ath_ID']).any()):
-        #athlete not found!
-        partsh.append([entry['Ath_ID'], entry['Name'], entry['YOB'], entry['Gender'], entry['Nation']])
+        rcs.loc[race_id] =  [race_id, evid, race_distance, race_year] #update our internal race list
+
+    # Now to add participants and results -- for each item in the result table:
+    # 1) Check the participant table to see if we want to write a new participant entry based on the ath_id, if we do then write it
+    # 2) Check the results table for race id + ath_id pairs. if not found, write a result entry.
+    resid = 1
+    for i in range(len(results_info)): #for each result -- we first handle adding the participant, then adding their result
+        entry = results_info.iloc[i]
     
-    #does the result table already have this specific result for this athlete? (race id + ath_id pair)
-    if resdf['Race_ID'].eq(evid).any(): #race has entries already
-        if not resdf[resdf['Race_ID'] == evid]['ath_id'].eq(entry['Ath_ID']).any(): #zero in on those entries, is this athlete yet among them?
-            #no, so let's add them
-            
-            
-            if( len(resdf['id']) > 1 ):
-                resid = max(max(resdf['id'][1:]), resid) + 1   
-            else:
-                resid = 1       
+        #does that result's participant already exist in our dataset?
+        if(not partdf['ath_id'].eq(entry['Ath_ID']).any()):
+            #athlete not found! Let's add them
+            partsh.append([entry['Ath_ID'], entry['Name'], entry['YOB'], entry['Gender'], entry['Nation']])
+    
+        #does the result table already have this specific result for this athlete? (race id + ath_id pair)
+        if resdf['race_id'].eq(evid).any(): #does this race have entries already in general?
+            #it does, look for the athlete
+            if not resdf[resdf['race_id'] == evid]['ath_id'].eq(entry['Ath_ID']).any(): #zero in on those entries, is this athlete yet among them?
+                #no, so let's add them       
+                if( len(resdf['result_id']) > 1 ):
+                    resid = max(max(resdf['result_id'][1:]), resid) + 1   
+                else:
+                    resid = 1       
+                ressh.append([resid, evid, entry['Ath_ID'], entry['Duration'], entry['Rank'], entry['Category'], entry['Cat_Rank'], entry['Gender_Rank']])
 
-            ressh.append([resid, evid, entry['Ath_ID'], entry['Duration'], entry['Rank'], entry['Category'], entry['Cat_Rank'], entry['Gender_Rank']])
+        else: #this race does not have any entries, so let's add new entries for this race
+                ressh.append([resid, evid, entry['Ath_ID'], entry['Duration'], entry['Rank'], entry['Category'], entry['Cat_Rank'], entry['Gender_Rank']])
+    wb.save(path)
 
-
-wb.save(path)
+#url = 'https://statistik.d-u-v.org/getresultevent.php?event=104431'
+#single_event_scrape(url)
